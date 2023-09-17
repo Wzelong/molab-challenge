536e19ab6c45f46d3dd323808219a2011695bd60
Zelong Wang <wang2447@wisc.edu>
2023-09-17 02:19:06 -0700

    dumb mistake

--- server/routes/articleRoutes.js
+++ server/routes/articleRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const ArticleController = require("../controllers/ArticleController");
+const multer = require("multer");
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
